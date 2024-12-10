db3759e9926f3dad427276725051ea4603970311
Ömer Koyuncu <omerkoyuncutr@gmail.com>
2024-12-10 14:24:46 +0300

    BooksController post, put, delete istekleri eklendi. 6.8

--- bsStoreApp/WebApi/Controllers/BooksController.cs
+++ bsStoreApp/WebApi/Controllers/BooksController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
 using WebApi.Repositories;
 
 namespace WebApi.Controllers
@@ -28,5 +29,95 @@ namespace WebApi.Controllers
                 throw new Exception(ex.Message);
             }
         }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetOneBook([FromRoute(Name = "id")] int id)
+        {
+            try
+            {
+                var book = _context.Books.Where(b => b.Id.Equals(id)).SingleOrDefault();
+
+                if (book is null)
+                {
+                    return NotFound();
+                }
+                return Ok(book);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult CreateOneBook([FromBody] Book book)
+        {
+            try
+            {
+                if (book is null)
+                {
+                    return BadRequest();
+                }
+
+                _context.Books.Add(book);
+                _context.SaveChanges();
+
+                return StatusCode(201, book);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("{id:int}")]
+        public IActionResult UpdateOneBook([FromRoute(Name = "id")] int id, [FromBody] Book book)
+        {
+            try
+            {
+                var entity = _context.Books.Where(b => b.Id.Equals(id)).SingleOrDefault();
+
+                if (entity is null)
+                    return NotFound();
+
+                if (id != book.Id)
+                    return BadRequest();
+
+                entity.Title = book.Title;
+                entity.Price = book.Price;
+
+                _context.SaveChanges();
+
+                return Ok(book);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult DeleteOneBook([FromRoute(Name = "id")] int id)
+        {
+            try
+            {
+                var entity = _context.Books.Where(b => b.Id.Equals(id)).SingleOrDefault();
+
+                if (entity is null)
+                {
+                    return NotFound(new { statusCode = 404, message = $"{id} id'li kitap bulunamadı." });
+                }
+
+                _context.Books.Remove(entity);
+                _context.SaveChanges();
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
     }
 }
