7895fe4141e1aaf7f98bfa1eb9056d9e7edf69bb
Ömer Koyuncu <omerkoyuncutr@gmail.com>
2024-12-10 14:14:28 +0300

    Books controller oluşturuldu. Get All Books httpget isteği eklendi. 6.7

---
+++ bsStoreApp/WebApi/Controllers/BooksController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Repositories;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly RepositoryContext _context;
+
+        public BooksController(RepositoryContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllBooks() 
+        {
+            try
+            {
+                var books = _context.Books.ToList();
+                return Ok(books);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
