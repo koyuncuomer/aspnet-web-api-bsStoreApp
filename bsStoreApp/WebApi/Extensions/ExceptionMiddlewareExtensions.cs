227652be70e73473e1c58133e713760f3cb387b7
Ömer Koyuncu <omerkoyuncutr@gmail.com>
2024-12-11 14:18:41 +0300

    Global hata yönetimi eklendi, yapılandırıldı. 9.2, 9.3, 9.4

---
+++ bsStoreApp/WebApi/Extensions/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using Entities.ErrorModel;
+using Microsoft.AspNetCore.Diagnostics;
+using Services.Contracts;
+
+namespace WebApi.Extensions
+{
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static void ConfigureExceptionHandler(this WebApplication app, ILoggerService logger)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "application/json";
+
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if (contextFeature is not null)
+                    {
+                        logger.LogError($"Hata ile karşılaşıldı: {contextFeature.Error}");
+                        await context.Response.WriteAsync(new ErrorDetails() {
+                            StatusCode = context.Response.StatusCode,
+                            Message = "Internal Server Error"
+                        }.ToString());
+                    }
+                });
+            });
+        }
+    }
+}
