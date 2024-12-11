2a17efbd2670dfc1a966d825b5ddb2b115204631
Ömer Koyuncu <omerkoyuncutr@gmail.com>
2024-12-11 22:32:02 +0300

    Alanların doğrulaması(validation) için giriş yapıldı. 12.3

---
+++ bsStoreApp/Entities/DataTransferObjects/BookDtoForManipulation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DataTransferObjects
+{
+    public abstract record BookDtoForManipulation
+    {
+        [Required(ErrorMessage = "Bu alan zorunludur.")]
+        [MinLength(2, ErrorMessage ="Minimum 2 karakter içermelidir.")]
+        [MaxLength(50, ErrorMessage = "Maksimum 50 karakter olabilir.")]
+        public String Title { get; init; }
+
+        [Required(ErrorMessage = "Bu alan zorunludur.")]
+        [Range(10, 1000)]
+        public decimal Price { get; init; }  
+    }
+}
