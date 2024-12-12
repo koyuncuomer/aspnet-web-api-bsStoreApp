ff65c2f26a7ba373408ebfdfdd356c3a3d5b922f
Ömer Koyuncu <omerkoyuncutr@gmail.com>
2024-12-12 22:44:36 +0300

    Data Shaping için katmanlarda gerekli düzeltmeler yapıldı. 19.6

--- bsStoreApp/Services/ServiceManager.cs
+++ bsStoreApp/Services/ServiceManager.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
+using Entities.DataTransferObjects;
 using Repositories.Contracts;
 using Services.Contracts;
 
@@ -12,9 +13,9 @@ namespace Services
     public class ServiceManager : IServiceManager
     {
         private readonly Lazy<IBookService> _bookService;
-        public ServiceManager(IRepositoryManager repositoryManager, ILoggerService logger, IMapper mapper)
+        public ServiceManager(IRepositoryManager repositoryManager, ILoggerService logger, IMapper mapper, IDataShaper<BookDto> shaper)
         {
-            _bookService = new Lazy<IBookService>(() => new BookManager(repositoryManager, logger, mapper));
+            _bookService = new Lazy<IBookService>(() => new BookManager(repositoryManager, logger, mapper, shaper));
         }
         public IBookService BookService => _bookService.Value;
     }
