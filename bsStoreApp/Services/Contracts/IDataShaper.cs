6a61ecee0ee8a0a81f4b194b1f1012ee8eed5457
Ömer Koyuncu <omerkoyuncutr@gmail.com>
2024-12-13 12:48:50 +0300

    Hypermedia desteği için gerekli başlangıç sınıfları tanımlandı. 20.2, 20.3, 20.4, 20.5, 20.6, 20.7

--- bsStoreApp/Services/Contracts/IDataShaper.cs
+++ bsStoreApp/Services/Contracts/IDataShaper.cs
@@ -4,12 +4,13 @@ using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Entities.Models;
 
 namespace Services.Contracts
 {
     public interface IDataShaper<T>
     {
-        IEnumerable<ExpandoObject> ShapeData(IEnumerable<T> entities, string fieldsString);
-        ExpandoObject ShapeData (T entity, string fieldsString);
+        IEnumerable<ShapedEntity> ShapeData(IEnumerable<T> entities, string fieldsString);
+        ShapedEntity ShapeData (T entity, string fieldsString);
     }
 }
