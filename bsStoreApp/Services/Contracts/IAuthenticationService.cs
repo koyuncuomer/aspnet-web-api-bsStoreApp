8602227815d8d5d05a4ff542ba5359f00c972b84
Ömer Koyuncu <omerkoyuncutr@gmail.com>
2024-12-14 16:39:50 +0300

    Authentication interface ve sınıfı tanımlandı. 26.11, 26.12, 26.13

---
+++ bsStoreApp/Services/Contracts/IAuthenticationService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.DataTransferObjects;
+using Microsoft.AspNetCore.Identity;
+
+namespace Services.Contracts
+{
+    public interface IAuthenticationService
+    {
+        Task<IdentityResult> RegisterUser(UserForRegistrationDto userForRegistrationDto);
+    }
+}
--- bsStoreApp/Services/ServiceManager.cs
+++ bsStoreApp/Services/ServiceManager.cs
@@ -5,6 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
 using Entities.DataTransferObjects;
+using Entities.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 using Repositories.Contracts;
 using Services.Contracts;
 
@@ -13,10 +16,22 @@ namespace Services
     public class ServiceManager : IServiceManager
     {
         private readonly Lazy<IBookService> _bookService;
-        public ServiceManager(IRepositoryManager repositoryManager, ILoggerService logger, IMapper mapper, IBookLinks bookLinks)
+        private readonly Lazy<IAuthenticationService> _authenticationService;
+        public ServiceManager(IRepositoryManager repositoryManager, 
+            ILoggerService logger, 
+            IMapper mapper, 
+            UserManager<User> userManager,
+            IConfiguration configuration,
+            IBookLinks bookLinks)
         {
             _bookService = new Lazy<IBookService>(() => new BookManager(repositoryManager, logger, mapper, bookLinks));
+
+            _authenticationService = new Lazy<IAuthenticationService>(() => 
+            new AuthenticationManager(logger, mapper, userManager, configuration));
         }
+
         public IBookService BookService => _bookService.Value;
+
+        public IAuthenticationService AuthenticationService => _authenticationService.Value;
     }
 }
