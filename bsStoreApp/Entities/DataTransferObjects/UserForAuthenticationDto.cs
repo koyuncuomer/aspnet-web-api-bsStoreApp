c4f9f6825b64c77673f76aa0be386cfd346a1467
Ömer Koyuncu <omerkoyuncutr@gmail.com>
2024-12-14 18:29:57 +0300

    Login işleminde user doğrulama için tanımlamalar yapıldı. 26.19, 26.20, 26.21

---
+++ bsStoreApp/Entities/DataTransferObjects/UserForAuthenticationDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DataTransferObjects
+{
+    public record UserForAuthenticationDto
+    {
+        [Required(ErrorMessage = "Username zorunlu alan.")]
+        public string? UserName { get; init; }
+
+        [Required(ErrorMessage = "Password zorunlu alan.")]
+        public string? Password { get; init; }
+    }
+}
