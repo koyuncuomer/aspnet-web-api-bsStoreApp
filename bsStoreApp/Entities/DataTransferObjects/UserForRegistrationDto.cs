68410fe3a1d1b3704e3e207ace94a707dcfb1803
Ömer Koyuncu <omerkoyuncutr@gmail.com>
2024-12-14 16:06:07 +0300

    User kayıt için Dto tanımı yapıldı. 26.10

---
+++ bsStoreApp/Entities/DataTransferObjects/UserForRegistrationDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DataTransferObjects
+{
+    public record UserForRegistrationDto
+    {
+        public string? FirsName { get; init; }
+        public string? LastName { get; init; }
+
+        [Required(ErrorMessage = "Kullanıcı Adı zorunlu alan.")]
+        public string? UserName { get; init; }
+
+        [Required(ErrorMessage = "Parola zorunlu alan.")]
+        public string? Password { get; init; }
+
+        public string? Email { get; init; }
+        public string? PhoneNumber { get; init; }
+
+        public ICollection<string>? Roles { get; init; }
+
+
+
+    }
+}
