5d31401c97f2ef1ed721ed0d2aa052090ed93fdb
Ömer Koyuncu <omerkoyuncutr@gmail.com>
2024-12-15 16:35:48 +0300

    Category için Repositories altında tanımlamalar yapıldı. 28.4, 28.5, 28.6, 28.7

---
+++ bsStoreApp/Repositories/Contracts/ICategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Models;
+
+namespace Repositories.Contracts
+{
+    public interface ICategoryRepository : IRepositoryBase<Category>
+    {
+        // IRepositoryBase'den gelen metodlar direkt kullanılabilir. Onun yerine kendi metodları tanımlanacak.
+        Task<IEnumerable<Category>> GetAllCategoriesAsync(bool trackChanges);
+        Task<Category> GetOneCategoryByIdAsync(int id, bool trackChanges);
+        void CreateOneCategory(Category category);
+        void DeleteOneCategory(Category category);
+        void UpdateOneCategory(Category category);
+    }
+}
---
+++ bsStoreApp/Repositories/EFCore/Config/CategoryConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Repositories.EFCore.Config
+{
+    public class CategoryConfig : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(c => c.Id);
+            builder.Property(c => c.Name).IsRequired();
+
+            builder.HasData(new Category() { Id = 1, Name = "Kategori 1" });
+        }
+    }
+}
